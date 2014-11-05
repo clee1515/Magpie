ebfe98470c6036a6509c0ad22a4c88d43e2035bf
clee1515 <clee15@punahou.edu>
2014-11-04 22:53:35 -1000

    Magpie Activity 2
    Added all the components needed to complete the pie of maggie activity 2

--- MagpieRunner.java
+++ MagpieRunner.java
@@ -1,13 +1,7 @@
 import java.util.Scanner;
 
-/**
- * A simple class to run the Magpie class.
- * @author Laurie White
- * @version April 2012
- */
 public class MagpieRunner
 {
-
  /**
   * Create a Magpie, give it user input, and print its replies.
   */
