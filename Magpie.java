e861a9f0ee23382f1446e2787fc8379da2901860
clee1515 <clee15@punahou.edu>
2014-11-06 12:27:58 -1000

    Magpie Activity 3
    Added in the findKeyword method to address the problem of keywords
    within other words

--- Magpie.java
+++ Magpie.java
@@ -18,6 +18,74 @@ public class Magpie
     return "Hello, let's talk.";
   }
   
+   private int findKeyword(String statement, String goal,
+   int startPos)
+ {
+  String phrase = statement.trim();
+  // The only change to incorporate the startPos is in
+  // the line below
+  int psn = phrase.toLowerCase().indexOf(
+    goal.toLowerCase(), startPos);
+
+  // Refinement--make sure the goal isn't part of a
+  // word
+  while (psn >= 0)
+  {
+   // Find the string of length 1 before and after
+   // the word
+   String before = " ", after = " ";
+   if (psn > 0)
+   {
+    before = phrase.substring(psn - 1, psn)
+      .toLowerCase();
+   }
+   if (psn + goal.length() < phrase.length())
+   {
+    after = phrase.substring(
+      psn + goal.length(),
+      psn + goal.length() + 1)
+      .toLowerCase();
+   }
+
+   // If before and after aren't letters, we've
+   // found the word
+   if (((before.compareTo("a") < 0) || (before
+     .compareTo("z") > 0)) // before is not a
+           // letter
+     && ((after.compareTo("a") < 0) || (after
+       .compareTo("z") > 0)))
+   {
+    return psn;
+   }
+
+   // The last position didn't work, so let's find
+   // the next, if there is one.
+   psn = phrase.indexOf(goal.toLowerCase(),
+     psn + 1);
+
+  }
+
+  return -1;
+ }
+
+ /**
+  * Search for one word in phrase. The search is not case
+  * sensitive. This method will check that the given goal
+  * is not a substring of a longer string (so, for
+  * example, "I know" does not contain "no"). The search
+  * begins at the beginning of the string.
+  * 
+  * @param statement
+  *            the string to search
+  * @param goal
+  *            the string to search for
+  * @return the index of the first occurrence of goal in
+  *         statement or -1 if it's not found
+  */
+ private int findKeyword(String statement, String goal)
+ {
+  return findKeyword(statement, goal, 0);
+ }
   /**
    * Gives a response to a user statement
    * 
@@ -33,37 +101,37 @@ public class Magpie
       response = "Cmon don't be scared, type something!";
     }
     
-    else if (statement.indexOf("no") >= 0)
+    else if (findKeyword(statement,"no") >= 0)
     {
       response = "Why so negative?";
     }
-    else if (statement.indexOf("mother") >= 0
-               || statement.indexOf("father") >= 0
-               || statement.indexOf("sister") >= 0
-               || statement.indexOf("brother") >= 0)
+    else if (findKeyword(statement,"mother") >= 0
+               || findKeyword(statement,"father") >= 0
+               || findKeyword(statement,"sister") >= 0
+               || findKeyword(statement,"brother") >= 0)
     {
       response = "Tell me more about your family.";
     }
     
-     else if (statement.indexOf("superman") >= 0)
+     else if (findKeyword(statement,"superman") >= 0)
     {
       response = "Batman could kick his ass.";
     }
      
-     else if (statement.indexOf("ebola") >= 0)
+     else if (findKeyword(statement,"ebola") >= 0)
     {
       response = "Ebola scares me.";
     }
     
-      else if (statement.indexOf("minecraft") >= 0)
+      else if (findKeyword(statement,"minecraft") >= 0)
     {
       response = "I love that game.";
     }
       
-    else if (statement.indexOf("dog") >= 0
-               || statement.indexOf("cat") >= 0
-               || statement.indexOf("fish") >= 0
-               || statement.indexOf("bird") >= 0)
+    else if (findKeyword(statement,"dog") >= 0
+               || findKeyword(statement,"cat") >= 0
+               || findKeyword(statement,"fish") >= 0
+               || findKeyword(statement,"bird") >= 0)
     {
       response = "Tell me more about your pets.";
     }
